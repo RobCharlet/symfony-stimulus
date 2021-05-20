decbd882c7bd88ef9b888ae99627c6aebb321cb2
Robin Charlet <robin.charlet@laposte.net>
2021-05-20 14:59:06 +0200

    Use useTransition to handle search-preview fade-in-out

--- assets/controllers/search-preview_controller.js
+++ assets/controllers/search-preview_controller.js
@@ -1,6 +1,7 @@
 import {Controller}      from 'stimulus';
 import {useClickOutside} from 'stimulus-use';
 import {useDebounce}     from 'stimulus-use';
+import {useTransition}   from 'stimulus-use';
 
 export default class extends Controller {
     static values = {
@@ -9,31 +10,41 @@ export default class extends Controller {
 
     static targets = ['result'];
 
-    static debounces = ['search']
+    static debounces = ['search'];
 
     connect() {
         useClickOutside(this);
         useDebounce(this);
+        useTransition(this, {
+            element: this.resultTarget,
+            enterActive: 'fade-enter-active',
+            enterFrom: 'fade-enter-from',
+            enterTo: 'fade-enter-to',
+            leaveActive: 'fade-leave-active',
+            leaveFrom: 'fade-leave-from',
+            leaveTo: 'fade-leave-to',
+            hiddenClass: 'd-none',
+        });
     }
 
     onSearchInput(event) {
-        this.search(event.currentTarget.value)
+        this.search(event.currentTarget.value);
     }
 
     // debounce only work with functions we call herself
     // browser call onSearchInput() and prevent debounce
     // from working simultaneously
     async search(query) {
-        const params = new URLSearchParams({
+        const params                = new URLSearchParams({
             q      : query,
             preview: 1
         });
         const response              = await fetch(`${this.urlValue}?${params.toString()}`);
         this.resultTarget.innerHTML = await response.text();
+        this.enter();
     }
 
     clickOutside(event) {
-        // example to close a modal
-        this.resultTarget.innerHTML = '';
+        this.leave();
     }
 }
